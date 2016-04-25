3cfc2939cf762c5de2102ebe108381792e874796
riad <khr.1380@gmail.com>
2016-04-25 19:28:09 +0200

    include the tooltip part to shoe data when mouseover is added

--- readDataFromCSV/js/importFromCSV.js
+++ readDataFromCSV/js/importFromCSV.js
@@ -1,8 +1,14 @@
 // smooth zooming in d3.js , transform transition, contries
+ // new line 3 
+//var separatezoneRecrangle = d3.select("body").append("div");
+var poly = d3.select("body").append("svg").attr("width",1000).attr("height",667);
+var tooltip = d3.select("body").append('div').attr("class", "tooltip").style("display","none");
 var zoom = d3.behavior.zoom().scaleExtent ([1, 10]).on ("zoom", zoomed);
-var poly = d3.select("body").append("svg").attr("width",1000).attr("height",667).append("g").call(zoom);
-var container = poly.append("g");
-
+var container = poly.append("g").call(zoom);
+// more or less adding html
+//separatezoneRecrangle.style("height","40px").style("width","60px");
+//separatezoneRecrangle.html('<p> dnddf </p>');
+//separatezoneRecrangle.append("g");
 var xydata = function (d){
 	return {
 		id: d.id,
@@ -48,26 +54,49 @@ var projectDatas =d3.csv("node.csv",xydata,function(data) {
 		polygonDatas [i] = links.slice(); 
 	}
 
+	console.log(polygonDatas);
+	console.log(polygonDatas [0] );
+	console.log(polygonDatas [0] [0] .id);
 	 container.selectAll("g")
-			.data(polygonDatas)
+			.data(polygonDatas) // polygon data hole array of 4 objects, where each object has id, x and y
 			.enter()
 			.append("polygon")
-			.attr("points",function(d){
-				return d.map(function(d){ 
+			.attr("points",function(d){ // ekhane d hole each polygondatas
+				return d.map(function(d){ // ekhane d holo each polygon datas [i]
 					return [scaleX(d.x),scaleY(d.y)].join(",");
 					})
 				.join(" ");
 				})
 			.attr("stroke","blue")
 			.attr("fill","red")
-			.attr("stroke-width",2);
+			.attr("stroke-width",2)
+			.on('mouseover',function (d){ 
+				d.forEach(function (d){
+					tooltip.transition()
+							.duration(200)
+							.style("display","inline");
+					tooltip.text(d.x +',' + d.y )
+							.style("left",(d3.event.pageX) +"px")
+							.style("top",(d3.event.pageY) + "px");
+				})
+			})
+			.on('mouseout',function(d){
+				div.style("display","none");	
+			})
+			//.on('click',function(d){
+			//	console.log();
+			//})
+
+
 });
 });
+
+
+
 
 function zoomed () {
 	container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
-
 
 
 
